68e5bac9b7c4a4cdd0ca91965220ac05ef334279
elterribleabuelo <rguerreroh@uni.pe>
2022-02-16 13:19:19 -0500

    Poll component logic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false; // Paso 1. Mostrar el formulario de nueva encuesta al clickear
+
+  // Datos de prueba
+  polls = [{
+    question : 'Te gustan los perros o los gatos?',
+    image : 'https://images.pexels.com/photos/7527363/pexels-photo-7527363.jpeg',
+    votes: [0,5,7],
+    voted: true,
+  },
+  {
+    question : 'Cual es el mejor mes para las vacaciones de verano?',
+    image : 'https://images.pexels.com/photos/4577186/pexels-photo-4577186.jpeg',
+    votes: [1,6,4],
+    voted: true,
+  }];
 }
--- frontend/src/app/poll/poll.component.ts
+++ frontend/src/app/poll/poll.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-poll',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PollComponent implements OnInit {
 
-  constructor() { }
+  @Input() question: string;
+  @Input() votes: number[]; //[0,1,5,7]
+  @Input() voted: boolean;
+  @Input() pollImage:string;
+
+  // Propiedades
+  numberOfVotes: number;
+
+  constructor() {
+   }
 
   ngOnInit(): void {
+    //  Hallando el número total de votos
+    if(this.votes.length){
+      this.numberOfVotes = this.votes.reduce((acc, curr) => {
+        // Corrida 1  : acc = 0 , curr = 0 -> acc = 0
+        // Corrida 2  : acc = 0 , curr = 1 -> acc = 1
+        // Corrida 3  : acc= 1  , curr = 5 -> acc = 6
+        // Corrida 4  : acc = 6 , curr = 7 -> acc = 13
+        return acc += curr
+      })
+    }
   }
 
 }
