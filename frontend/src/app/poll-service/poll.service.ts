58912c9b500f353b51a12b6b2921d4fadc84d4da
elterribleabuelo <rguerreroh@uni.pe>
2022-02-23 16:23:21 -0500

    Normalizando los datos

--- frontend/src/app/poll-service/poll.service.ts
+++ frontend/src/app/poll-service/poll.service.ts
@@ -3,12 +3,15 @@ import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { Web3Service } from '../blockchain/web3.service';
 import { Poll, PollForm } from '../types';
-import {fromAscii} from 'web3-utils';
+import {fromAscii,toAscii} from 'web3-utils';
+import { normalize } from 'path';
 
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class PollService {
 
   constructor(private web3:Web3Service) { }
@@ -24,16 +27,16 @@ export class PollService {
 
     // Parametros
     const acc = await this.web3.getAccount(); // hash de la cuenta de memtamask
-    const voter = await this.web3.call('getVoter',acc);
+    const voter = await this.web3.call('getVoter',acc); // voter => [voters[_id].id, voters[_id].votedIds]
+    const voterNormalize = this.normalizeVoter(voter); // valores de voter en formato JSON
+
 
     for (let i = 0; i < totalPolls; i++){
-      const poll = await this.web3.call('getPoll',i);
-      polls.push(poll);
+      const pollRaw = await this.web3.call('getPoll',i); //pollRaw: tiene forma de arreglo
+      const pollNormalized = this.normalizePoll(pollRaw,voterNormalize); //
+      polls.push(pollNormalized);
     }
-
     return polls;
-
-
   }
 
   vote(pollId: number, voteNumber: number){
@@ -52,5 +55,28 @@ export class PollService {
     console.log("Poll creado en el servicio:", poll);
 
     // JSON: {question: 'hola que tal', thumbnail: 'https://...,options:["a","b","c"]}
+  }
+
+  private normalizeVoter(voter){
+    return {
+      id: voter[0],
+      votedIds: voter[1].map(vote => parseInt(vote))
+    }
+  }
+
+  private normalizePoll(pollRaw,voter):Poll{
+    return{
+
+      id: parseInt(pollRaw[0]),
+      question:pollRaw[1],
+      thumbnail:pollRaw[2],
+      results: pollRaw[3].map((vote) => parseInt(vote)),
+      options:pollRaw[4].map(opt => toAscii(opt).replace(/\u0000/g,"")),
+      voted:
+        voter.votedIds.length &&
+        voter.votedIds.find(votedId => votedId == parseInt(pollRaw[0])) !=
+        undefined,
+
+    }
   }
 }
